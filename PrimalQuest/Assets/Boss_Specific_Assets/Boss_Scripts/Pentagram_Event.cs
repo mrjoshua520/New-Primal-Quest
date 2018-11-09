eb8757ab4d707c61482835c8a456ea88ede7fbb1
mrjoshua520 <joshua52099@gmail.com>
2018-11-09 03:15:54 -0500

    Portal Spawn Sequence
    Added a portal spawn sequence that happens when the player crosses a certain point. This is currently still a work in  progress but mostly done.

--- PrimalQuest/Assets/Boss_Specific_Assets/Boss_Scripts/Pentagram_Event.cs
+++ PrimalQuest/Assets/Boss_Specific_Assets/Boss_Scripts/Pentagram_Event.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pentagram_Event : MonoBehaviour {
+public class Pentagram_Event : MonoBehaviour
+{
+    Animator anim;
+    public GameObject light;
+    public GameObject circle;
+    public GameObject pentagram;
+    public GameObject pillar1;
+    public GameObject pillar2;
+    public GameObject pillar3;
+    public Material color;
+    bool triggered = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !triggered)
+        {
+            StartCoroutine(SpawnPortal());
+        }
+    }
+
+    IEnumerator SpawnPortal()
+    {
+        pentagram.SetActive(true);
+
+        light.SetActive(false);
+
+        RenderSettings.skybox = color;
+
+        yield return new WaitForSeconds(.5f);
+
+        pillar1.SetActive(true);
+
+        yield return new WaitForSeconds(1.25f);
+
+        pillar2.SetActive(true);
+
+        yield return new WaitForSeconds(1.25f);
+
+        pillar3.SetActive(true);
+
+        yield return new WaitForSeconds(5.5f);
+
+        circle.SetActive(true);
+    }
 }
