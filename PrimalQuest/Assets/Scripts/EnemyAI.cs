4240a27e69a5d2772706bb13ce394e08e4b9657c
Nick <webersoftware.dev@gmail.com>
2018-11-12 17:34:27 -0500

    AI
    Added in first basic AI elements

---
+++ PrimalQuest/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{   // to use functions from movement do this:
+    // public NPCMovement npcMovement;
+    // in Update
+    // if(!npcMovement.isWaiting)
+    // {
+    //    call functions from NPCMovement
+    // }  
+    
+
+    NavMeshAgent agent;
+    GameObject player;
+    Animator anim;
+
+    int range = 4;
+
+    public NPCMovement npcMovement;
+   
+	// Use this for initialization
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Vector3.Distance(player.transform.position, this.transform.position) < range)
+        {
+            npcMovement.SetTarget(agent, anim, player);
+            range = 100;
+        }        
+    }
+}
