a100b2c2eb5ed5751d850f1b83baf686548ecd55
mrjoshua520 <joshua52099@gmail.com>
2018-11-12 04:10:35 -0500

    Island Work
    Portal now summons. Object with the tag "Player" can travel from the island to the boos fight scene via the portal. Majority pf the island work is done other than details that will be added when I think of them and smothing out the transitions.

---
+++ PrimalQuest/Assets/Boss_Specific_Assets/Boss_Scripts/Portal_Summon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+/// <Notes>
+/// Need to add smooth transitions but this works for now
+/// </Notes>
+
+public class Portal_Summon : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject portal;
+    Vector3 dir;
+    Quaternion rot;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            portal.SetActive(true);
+            StartCoroutine(teleport());
+        }
+    }
+
+    IEnumerator teleport()
+    {
+        Player.transform.LookAt(portal.transform);
+
+        yield return new WaitForSeconds(7f);
+
+        SceneManager.LoadScene(4);
+    }
+}
