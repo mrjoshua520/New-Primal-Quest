26913921e72253e086c3e1638719857db07a7afd
mrjoshua520 <joshua52099@gmail.com>
2018-11-13 02:31:09 -0500

    Added Class Selection Scene
    Added a class selection scene and began basic layout of the player. (I will be doing most of the rest in a test scene out of the project before adding it in.

---
+++ PrimalQuest/Assets/Scripts/Player Scripts/Class_Selection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class_Selection : MonoBehaviour
+{
+    GameObject player;
+    
+    public void Archer()
+    {
+
+    }
+
+    public void Mage()
+    {
+
+    }
+
+    public void Warrior()
+    {
+
+    }
+}
---
+++ PrimalQuest/Assets/Scripts/Player Scripts/Player_Stats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Stats : MonoBehaviour
+{
+    string Class;
+    int health;
+
+    public void SetClass(string pClass)
+    {
+        Class = pClass;
+    }
+
+    void SetStats(string pClass)
+    {
+
+    }
+}
