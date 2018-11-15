f8aaf92b5937751cf69bd41de09fd149c4630ded
mrjoshua520 <joshua52099@gmail.com>
2018-11-15 02:39:09 -0500

    Archer Update
    Added the archer that has one animation to pull the bowstring back and release. Currently it doesn't shoot anything howver this update means the the base for all players is completed.

---
+++ PrimalQuest/Assets/PlayerAssets/Archer/ArrowShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowShoot : MonoBehaviour {
+
+    Animator anim;
+    bool isAttacking = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && isAttacking == false)
+        {
+            isAttacking = true;
+            StartCoroutine(Fire());
+            isAttacking = false;
+        }
+    }
+
+     IEnumerator Fire()
+    {
+        anim.SetBool("isAttack", true);
+
+        yield return new WaitForSeconds(.48f);
+
+        anim.SetBool("isAttack", false);
+    }
+}
---
+++ PrimalQuest/Assets/PlayerAssets/Player Scripts/Class_Selection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Class_Selection : MonoBehaviour
+{
+    GameObject player;
+    public GameObject mage;
+    public GameObject archer;
+    public GameObject warrior;
+    Vector3 loadpos;
+    static string Class;
+    
+    public void Archer()
+    {
+        mage.SetActive(false);
+        archer.SetActive(true);
+        warrior.SetActive(false);
+        Class = "Archer";
+        player = GameObject.Find("Archer");
+
+        DontDestroyOnLoad(player);
+        SceneManager.LoadScene(1);
+        loadpos = new Vector3(120f, 18f, 1f);
+        player.transform.position = loadpos;
+    }
+
+    public void Mage()
+    {
+        mage.SetActive(true);
+        archer.SetActive(true);
+        warrior.SetActive(false);
+        Class = "Mage";
+        player = GameObject.Find("Mage");
+
+        DontDestroyOnLoad(player);
+        SceneManager.LoadScene(1);
+        loadpos = new Vector3(120f, 18f, 1f);
+        player.transform.position = loadpos;
+    }
+
+    public void Warrior()
+    {
+        mage.SetActive(false);
+        archer.SetActive(false);
+        warrior.SetActive(true);
+        Class = "Warrior";
+        player = GameObject.Find("Warrior");
+
+        DontDestroyOnLoad(player);
+        SceneManager.LoadScene(1);
+        loadpos = new Vector3(120f, 18f, 1f);
+        player.transform.position = loadpos;
+    }
+
+    public string setClass()
+    {
+        return Class;
+    }
+}
