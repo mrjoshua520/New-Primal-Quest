d21bb117390db1d8bd5179dfc73f8735d3b910d9
AaronMasson <33638411+AaronMasson@users.noreply.github.com>
2018-11-17 18:09:47 -0500

    Added in Plant trigger and messed with enemies
    Enemies look good

---
+++ PrimalQuest/Assets/Scripts/PlantPickupTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantPickupTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("E key pressed");
+            }
+        }
+    }
+}
