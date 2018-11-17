0d41813dcefd3138df4e090af025e652f6fda242
JessMarimpietri <42822935+JessMarimpietri@users.noreply.github.com>
2018-11-16 22:47:53 -0500

    worked on animation of blacksmith
    Got the player to trigger the animation cycle. (Still need to work on actual good animation)

---
+++ PrimalQuest/Assets/Scripts/CityScripts/BlacksmithAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlacksmithAnimation : MonoBehaviour {
+
+    Animator anim;
+    public BlacksmithTrigger bst;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        //bst = GetComponent<BlacksmithTrigger>();
+        bst = FindObjectOfType<BlacksmithTrigger>();
+        
+
+        //StartCoroutine(blacksmithAnimation());
+    }
+
+    public IEnumerator blacksmithAnimation()
+    {
+        //bst.onTriggerBlSm = false;
+
+        Debug.Log("inside animation");
+
+        anim.SetBool("isWalking", false);
+        yield return new WaitForSeconds(5);
+        anim.SetBool("isWorking", true);
+        yield return new WaitForSeconds(7);
+        anim.SetBool("isSweating", true);
+        yield return new WaitForSeconds(2);
+        anim.SetBool("isSweating", false);
+        //yield return new WaitForSeconds(7);
+        //anim.SetBool("isWorking", false);
+
+        //bst.onTriggerBlSm = true;
+
+    }
+}
---
+++ PrimalQuest/Assets/Scripts/CityScripts/BlacksmithTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlacksmithTrigger : MonoBehaviour {
+
+    BlacksmithAnimation bsa;
+    //public bool onTriggerBlSm;
+
+    void Start()
+    {
+        bsa = FindObjectOfType<BlacksmithAnimation>();
+        //onTriggerBlSm = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("inside trigger player");
+            StartCoroutine(bsa.blacksmithAnimation());
+
+            //if (onTriggerBlSm == true)
+            //{
+                //Debug.Log("inside trigger player");
+                //StartCoroutine(bsa.blacksmithAnimation());
+            //}
+
+        }
+    }
+}
