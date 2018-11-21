b006e8a0410388b7d574f456946731560d9deee8
mrjoshua520 <joshua52099@gmail.com>
2018-11-20 23:20:08 -0500

    Dialogue Function
    Dialogue Function Implemented. Check google drive for the documentation on how to use it.

--- PrimalQuest/Assets/PlayerAssets/GUIScripts/HUD.cs
+++ PrimalQuest/Assets/PlayerAssets/GUIScripts/HUD.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class HUD : MonoBehaviour {
 
     Stats stat;
+    public GameObject panel;
+    public TextMeshProUGUI dialogue;
     public TextMeshProUGUI HP;
 
 	// Use this for initialization
@@ -19,15 +21,93 @@ public class HUD : MonoBehaviour {
 	void Update ()
     {
         DisplayHealth();
+        Test();
 	}
+
+    void Test()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Dialogue("Blacksmith", "Why hello there friend. This is a lovely test!!!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Dialogue("None", "Why hello there mortal. This is a lovely test!!!");
+        }
+    }
 
     void DisplayHealth()
     {
         HP.text = "Health: " + stat.GetHPPerc().ToString();
     }
 
-    void Dialogue(string text)
+    void Dialogue(string speaker, string text)
     {
+        int textLength = text.Length; //Length of the text sent
+        int speakerLength = speaker.Length; //Length of the speakers name
 
+        if (speaker == "None")
+        {
+            //For Narration there is no speaker
+            StartCoroutine(Display(text, textLength));
+        }
+        else
+        {
+            //For NPCs
+            StartCoroutine(Display(speaker, text, speakerLength, textLength));
+        }
+    }
+
+    IEnumerator Display(string speaker, string text, int speakerLength, int textLength)
+    {
+        char dispText;
+
+        panel.SetActive(true);
+
+        for (int i = 0; i < speakerLength; i++)
+        {
+            dispText = speaker[i];
+            dialogue.text += "" + dispText.ToString();
+            yield return new WaitForSeconds(.05f);
+        }
+
+        dialogue.text += ":";
+
+        yield return new WaitForSeconds(.05f);
+
+        dialogue.text += " ";
+
+        yield return new WaitForSeconds(.05f);
+
+        for (int i = 0; i < textLength; i++)
+        {
+            dispText = text[i];
+            dialogue.text += "" + dispText.ToString();
+            yield return new WaitForSeconds(.05f);
+        }
+
+        yield return new WaitForSeconds(5);
+        dialogue.text = "";
+        panel.SetActive(false);
+    }
+
+    IEnumerator Display(string text, int textLength)
+    {
+        char dispText;
+
+        panel.SetActive(true);
+
+        for (int i = 0; i < textLength; i++)
+        {
+            dispText = text[i];
+            Debug.Log(dispText);
+            dialogue.text += "" + dispText.ToString();
+            yield return new WaitForSeconds(.05f);
+        }
+
+        yield return new WaitForSeconds(5);
+        dialogue.text = "";
+        panel.SetActive(false);
     }
 }
