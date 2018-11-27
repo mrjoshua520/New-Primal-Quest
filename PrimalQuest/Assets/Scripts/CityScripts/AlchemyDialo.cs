a8ec198fe6bf78e3f24adfcf7805fce912a030e3
AaronMasson <33638411+AaronMasson@users.noreply.github.com>
2018-11-26 20:20:37 -0500

    Added in Dialogue and Trigger scripts for Alchemist
    Also added in some tents in the forest to trick player

---
+++ PrimalQuest/Assets/Scripts/CityScripts/AlchemyDialo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AlchemyDialo : MonoBehaviour {
+
+    EnemyAI enemyAI;
+    NavMeshAgent navMesh;
+    Animator anim;
+    GameObject player;
+    GameObject text;
+    PlayerHUD pHUD;
+	
+	void Start ()
+    {
+        enemyAI = GetComponent<EnemyAI>();
+        anim = GetComponent<Animator>();
+        text = GameObject.Find("HUD");
+        pHUD = text.GetComponent<PlayerHUD>();
+	}
+
+    public void setUp()
+    {
+        enemyAI.enabled = false;
+        navMesh.enabled = false;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.LookAt(player.transform);
+
+        anim.SetBool("isWalking", false);
+    }
+
+    public void dialogue()
+    {
+        Debug.Log("Scripts working together");
+        pHUD.Dialogue("Alchemist", "Hello Sir");
+    }
+	
+	
+}
---
+++ PrimalQuest/Assets/Scripts/CityScripts/AlchemyTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlchemyTrigger : MonoBehaviour {
+
+    public AlchemyDialo ALDI;
+    Collider collider;
+
+	void Start ()
+    {
+        collider = GetComponent<Collider>();
+	}
+	
+	
+    void OnTriggerEnter(Collider trigger)
+    {
+        if(trigger.tag == "Player")
+        {
+            Debug.Log("Player inside alchemy shop");
+            collider.enabled = false;
+
+        }
+    }
+}
