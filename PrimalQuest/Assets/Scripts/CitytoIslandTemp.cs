2f1b6fa5a0d666d28798098c3470c4e8e1754824
mrjoshua520 <joshua52099@gmail.com>
2018-11-29 06:40:45 -0500

    Temp Island travel
    Added in a temporary way to get to the island and fight the boss. Very close to having the final one however need an NPC for it. Will work on that after class

---
+++ PrimalQuest/Assets/Scripts/CitytoIslandTemp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CitytoIslandTemp : MonoBehaviour
+{
+
+    GameObject player;
+    Vector3 loadpos;
+    Stats Stat;
+
+    private void Start()
+    {
+        Stat = new Stats();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bool forest = Stat.GetForest();
+        bool cave = Stat.GetCave();
+
+        if (other.tag == "Player" && forest && cave)
+        {
+            player = GameObject.FindWithTag("Player");
+
+            DontDestroyOnLoad(player);
+            SceneManager.LoadScene(4);
+            loadpos = new Vector3(210f, 10f, 567.5f);
+            player.transform.position = loadpos;
+        }
+    }
+}
