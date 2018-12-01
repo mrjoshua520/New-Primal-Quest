d40a86c40a678f00cb7243a5ace77bb21430bbf5
JessMarimpietri <42822935+JessMarimpietri@users.noreply.github.com>
2018-11-30 22:42:31 -0500

    Knight Dialogue
    Still need to fix up the actual dialogue, but the player can talk to him.

---
+++ PrimalQuest/Assets/Scripts/CityScripts/VillagerDialogue.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class VillagerDialogue : MonoBehaviour
+{
+
+    EnemyAI enemyAI;
+    public VillagerTrigger vt;
+    GameObject player;
+    Animator anim;
+    GameObject text;
+    PlayerHUD pHUD;
+    Stats stats;
+    bool talkedToKnight2 = false;
+
+   
+    
+    void Start()
+    {
+        enemyAI = GetComponent<EnemyAI>();
+        anim = GetComponent<Animator>();
+        text = GameObject.Find("HUD");
+        pHUD = text.GetComponent<PlayerHUD>();
+        stats = new Stats();
+
+    }
+
+    public void disableMovementAndSetUpForDialogue()
+    {
+        if (gameObject.name == "Knight2" && !talkedToKnight2)
+        {
+            Debug.Log("inside disable movement");
+            enemyAI.wander = false;
+            Debug.Log("inside disable movement: after wander equals false");
+
+            player = GameObject.FindGameObjectWithTag("Player");
+            transform.LookAt(player.transform);
+
+            anim.SetBool("isWalking", false);
+            Debug.Log("inside disable movement: after anim, before cor");
+            StartCoroutine(villagerDialogue());
+        }
+        
+    }
+
+    public IEnumerator villagerDialogue()
+    {
+        if (gameObject.name == "Knight2")
+        {
+            pHUD.Dialogue("Knight", "Hello there. I just bought an amazing health potion from the alchemy shop. You should get some!");
+            Debug.Log("After dialogue");
+
+            yield return new WaitForSeconds(10);
+            Debug.Log("After wait for seconds");
+
+            talkedToKnight2 = true;
+            enableMovement();
+        }
+    }
+
+    void enableMovement()
+    {
+        Debug.Log("in enable movement");
+        enemyAI.wander = true;
+        //navmesh.enabled = true;
+
+        Debug.Log("After enable movement");
+
+        StartCoroutine(vt.turnOnCollider());
+    }
+}
---
+++ PrimalQuest/Assets/Scripts/CityScripts/VillagerTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class VillagerTrigger : MonoBehaviour
+{
+    Collider collider;
+    public VillagerDialogue vd;
+    
+    void Start()
+    {
+        collider = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("inside trigger player");
+
+            collider.enabled = false;
+            vd.disableMovementAndSetUpForDialogue();
+        }
+    }
+
+    public IEnumerator turnOnCollider()
+    {
+        Debug.Log("In turn of collider");
+        // to wait for villager to walk away from character
+        yield return new WaitForSeconds(10);
+        collider.enabled = true;
+        Debug.Log("After turn on collider");
+    }
+}
