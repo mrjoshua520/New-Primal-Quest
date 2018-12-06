9514a6047784cde4c56153822d9eb52bf0dbf011
mrjoshua520 <joshua52099@gmail.com>
2018-12-06 00:44:52 -0500

    Altar Interaction Finished
    Finished player interaction with the Island Altars

--- PrimalQuest/Assets/Boss_Specific_Assets/Boss_Scripts/Altar_Drak.cs
+++ PrimalQuest/Assets/Boss_Specific_Assets/Boss_Scripts/Altar_Drak.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Altar_Drak : MonoBehaviour
 {
+    Stats stat;
+
+    public GameObject light1;
+    public GameObject light2;
+    public GameObject light3;
+    public GameObject light4;
+    public GameObject Active;
+
+    private void Start()
+    {
+        stat = new Stats();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-
+        if (other.tag == "Player")
+        {
+            light1.SetActive(true);
+            light2.SetActive(true);
+            light3.SetActive(true);
+            light4.SetActive(true);
+        }
     }
 
     void OnTriggerStay(Collider collider)
@@ -15,8 +34,21 @@ public class Altar_Drak : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("Collect"))
             {
-                //Choose Uara
+                stat.SetSpec(3);
+                Active.SetActive(true);
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            light1.SetActive(false);
+            light2.SetActive(false);
+            light3.SetActive(false);
+            light4.SetActive(false);
+            Active.SetActive(false);
         }
     }
 }
